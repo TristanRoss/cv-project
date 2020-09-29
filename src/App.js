ce44071b621b789789ec1d4176b70628891bdc92
TristanRoss <tgr1998@live.com>
2020-09-28 21:10:48 -0500

    Add Form.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Form from './components/Form';
+import { Button } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+export class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      showForm: false,
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({showForm: !this.state.showForm})
+  }
+
+  render() {
+
+    const buttonStyle = {
+      margin: "10px",
+    }
+
+    return (
+      <div>
+        { this.state.showForm && (<Form></Form>) }
+        { this.state.showForm === false ? (<Button style={buttonStyle} color="primary" onClick={this.handleClick}>Enter information</Button>) : (<Button style={buttonStyle} color="primary" onClick={this.handleClick}>Close</Button>) }
+        
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
