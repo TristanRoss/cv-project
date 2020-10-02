d8d7a173e1be054d9e5745dbd89d60dfdb42b346
TristanRoss <tgr1998@live.com>
2020-10-02 17:54:44 -0500

    Updates

---
+++ precache-manifest.2681701d3acefafb035b1c2c9d862b23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2efd52f9a31a7ef706864c9c8991478c",
+    "url": "/cv-project/index.html"
+  },
+  {
+    "revision": "9fb2e8e59bba12c6e9fa",
+    "url": "/cv-project/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9fb2e8e59bba12c6e9fa",
+    "url": "/cv-project/static/js/2.b0b37971.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/cv-project/static/js/2.b0b37971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cc63881a3c6cc8effb2",
+    "url": "/cv-project/static/js/main.ae3acd10.chunk.js"
+  },
+  {
+    "revision": "f1ebb3293288666c5950",
+    "url": "/cv-project/static/js/runtime-main.d91c792d.js"
+  }
+]);
